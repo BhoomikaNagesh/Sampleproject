8b607e6ec22a10a116b00f38cd4f312741ae361e
BhoomikaNagesh <bhoomikaknb1105@gmail.com>
2023-01-29 13:35:27 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,22 @@
+print("Hello world")
+x = "RV College of Engineering"
+print(x)
+print(x[3])
+print(x[0])
+print(x + " " + "Bengaluru")
+print(str.lower(x))
+print(str.upper(x))
+a = "GSSS Institute of Engineering for women, KRS Road, Mysuru"
+y = a.split(",")
+print(y)
+b = "GSSS Institute of Engineering for women* KRS Road* Mysuru"
+y = b.split("*")
+print(y)
+a = 5
+print("Type of a:", type(a))
+b = 10.1
+print("Type of b:" , type(b))
+c = 1 + 10j
+print("Type of c:" , type(c))
+d = "ABCDE"
+print(type(d))
